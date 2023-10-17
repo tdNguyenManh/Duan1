67f6a01d351c02ffb765da0d3992a049e14d5605
tdNguyenManh <h19.it.ms.tn@gmail.com>
2023-10-17 17:11:54 +0700

    video23

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import DisplayInfor from './DisplayInfor';
 import UserInfor from './UserInfor';
 class MyComponent extends React.Component {
 
    
     render() {
+        const myInfor =['ab','c','c']
         return (
             <div>
  
                 <UserInfor />
-            </div>
-
+                <DisplayInfor/>
+                <DisplayInfor name="Nguyen Tuan Manh" age="19"/>
+                <hr/>
+                <DisplayInfor name={"Manh"} age={26} myInfor={myInfor}/>
+            </div>   
         );
     }
 }
