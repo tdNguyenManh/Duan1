085dcb73cb68573052925bbeb6c94cb92c7dc502
tdNguyenManh <h19.it.ms.tn@gmail.com>
2023-10-23 17:53:40 +0700

    video 51

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,7 +7,8 @@ const Header =()=> {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Tuan Manh & Manh</Navbar.Brand>
+        {/* <Navbar.Brand href="#home">Tuan Manh & Manh</Navbar.Brand> */}
+        <NavLink to='/' className='navbar-brand'>Nguyễn Tuấn Mạnh</NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
@@ -22,11 +23,9 @@ const Header =()=> {
           </Nav>
           <Nav>
           <NavDropdown title="Settings" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Log in</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Log out
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
+              <NavDropdown.Item >Log in</NavDropdown.Item>
+              <NavDropdown.Item >Log out</NavDropdown.Item>
+              <NavDropdown.Item >Profile</NavDropdown.Item>
               
             </NavDropdown>
           </Nav>
